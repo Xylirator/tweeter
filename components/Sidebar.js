86d3714dffc0fbd3f2900e6304d426eac3f24e0e
umar khan <umar.khan@student.uts.edu.au>
2023-03-05 01:39:20 +1100

    Added sidebar icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,9 +1,30 @@
 import Image from "next/image";
+import { HomeIcon } from "@heroicons/react/24/solid";
+import SidebarLink from "./SidebarLink";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ListBulletIcon,
+  UserIcon,
+  EllipsisHorizontalCircleIcon,
+} from "@heroicons/react/24/outline";
 function Sidebar() {
   return (
     <div className="hidden sm:flex flex-col items-center xl:item-start xl:w-[340px] p-2 fixed h-full">
       <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
-        <img src='https://rb.gy/ogau5a' width={30} height={30}/>
+        <Image src="/twitter-icon-svg-28.jpg" width={30} height={30} />
+      </div>
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ListBulletIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={EllipsisHorizontalCircleIcon} />
       </div>
     </div>
   );

43dbfc8de367dc77ed1f3b259155911bbecc3b94
umar khan <umar.khan@student.uts.edu.au>
2023-04-03 16:32:46 +1000

    Added nextauth

--- pages/index.js
+++ pages/index.js
@@ -18,4 +18,21 @@ const Home = () => {
     </>
   );
 };
-export default Home;
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/n/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getServerSession(context);
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
